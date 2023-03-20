a45035c12619b79e18cc082b64a2d0b5baa3f85f
AJITH1509 <ajithmuthu799@gmail.com>
2023-03-20 23:41:34 +0530

    dashboard added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,94 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Fade from "@mui/material/Fade";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import { UrlShortener } from "./UrlShortener";
+import { LoginPage } from "./LoginPage";
+import { SignUpPage } from "./SignUpPage";
+import { ForgetPass } from "./Forget";
+import { VerifyOtp } from "./VerifyOtp";
+import { NewPassword } from "./newPassword";
+import { EmailVerification } from "./EmailVerification";
 
 function App() {
-
+  const navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const token = localStorage.getItem("token");
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  function logOut() {
+    localStorage.clear();
+    navigate("/");
+  }
   return (
     <div className="App">
-      <UrlShortener />
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar color="secondary" position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Mini Link
+            </Typography>
+            <Button
+              id="fade-button"
+              aria-controls={open ? "fade-menu" : undefined}
+              aria-haspopup="true"
+              aria-expanded={open ? "true" : undefined}
+              onClick={handleClick}
+            >
+              <IconButton
+                size="large"
+                edge="start"
+                color="white"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+            </Button>
+            <Menu
+              id="fade-menu"
+              MenuListProps={{
+                "aria-labelledby": "fade-button",
+              }}
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              TransitionComponent={Fade}
+            >
+              <MenuItem onClick={handleClose}>Profile</MenuItem>
+              {token ? (
+                <MenuItem onClick={logOut}>Logout</MenuItem>
+              ) : (
+                <MenuItem onClick={() => navigate("/")}> Login</MenuItem>
+              )}
+            </Menu>
+          </Toolbar>
+        </AppBar>
+      </Box>
+
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        {/* <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login/forgetpassword" element={<ForgetPass />} />
+        <Route path="/verifyotp" element={<VerifyOtp />} />
+        <Route path="/mailverification" element={<EmailVerification />} /> */}
+        {/* <Route path="/setpassword" element={<NewPassword />} /> */}
+        <Route path="/urlshortener" element={<UrlShortener />} />
+      </Routes>
     </div>
   );
 }
--- src/UrlShortener.jsx
+++ src/UrlShortener.jsx
@@ -6,16 +6,38 @@ import Button from "@mui/material/Button";
 import { useState, useEffect } from "react";
 import { API } from "../global";
 
+function checkFunction(response) {
+  if (response.status === 401) {
+    throw Error("unauthorized");
+  } else {
+    return response.json();
+  }
+}
+
+function logout() {
+  localStorage.clear();
+  window.location.href = "/";
+}
+
 export const UrlShortener = () => {
   const [url, setUrl] = useState("");
-
   const [data, setData] = useState([]);
+  const [show, setShow] = useState(false);
   const getUrls = () => {
-    fetch(`${API}/urls`)
-      .then((response) => response.json())
-      .then((result) => setData(result));
+    fetch(`${API}/urls`, {
+      headers: {
+        "x-auth-token": localStorage.getItem("token"),
+      },
+    })
+      .then((response) => checkFunction(response))
+      .then((data) => setData(data))
+      .catch((err) => logout());
   };
-  useEffect(() => getUrls(), []);
+  useEffect(() => {
+    if (show) {
+      getUrls();
+    }
+  }, [show]);
 
   const handleClick = () => {
     fetch(`${API}/shorten`, {
@@ -23,6 +45,7 @@ export const UrlShortener = () => {
       body: JSON.stringify({ url }),
       headers: {
         "content-type": "application/json",
+        "x-auth-token": localStorage.getItem("token"),
       },
     }).then(() => getUrls());
   };
@@ -31,12 +54,20 @@ export const UrlShortener = () => {
       <Card className="input-container">
         <CardContent className="input">
           <TextField
+            value={url}
             onChange={(e) => setUrl(e.target.value)}
             label="Url"
             id="fullWidth"
           />
           <CardActions className="btn-actions">
-            <Button onClick={handleClick} color="success" variant="contained">
+            <Button
+              onClick={() => {
+                handleClick();
+                setShow(true);
+              }}
+              color="success"
+              variant="contained"
+            >
               Convert
             </Button>
           </CardActions>
@@ -53,8 +84,8 @@ export const UrlShortener = () => {
 
 const Url = ({ shortUrl }) => {
   return (
-    <div>
+    <Card className="url-container">
       <a href={`${API}/${shortUrl}`}>{`${API}/${shortUrl}`}</a>
-    </div>
+    </Card>
   );
 };
